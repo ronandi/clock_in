7774f9207e02bb24eb9d138ac6a863a852d2d818
Rohit Kumar <rohit.kumar@rutgers.edu>
2012-11-06 16:06:09 -0500

    implement init and update functions of timelog

--- clockin.js
+++ clockin.js
@@ -1,5 +1,12 @@
 #! /usr/bin/env node
 
+/*
+ * clockin - A very simple app for logging how long you've been working on
+ * a project
+ * Author: Rohit Kumar
+ * rohit.kumar@rutgers.edu
+ */
+
 var fs = require('fs');
 var readline = require('readline');
 var rl = readline.createInterface({
@@ -9,13 +16,37 @@ var rl = readline.createInterface({
 var TIMELOG = "timelog.txt";
 
 fs.exists(TIMELOG, function(exists) {
+    //if file does not exist. ask whether we should make one
     if (!exists) {
         rl.question("timelog not found. create new (y/n)?\n", function(answer) {
-            if (answer == 'y') {
-                console.log("timelog created. time logged");
-            } 
-            process.exit(0);
+            if (answer == "y") {
+                //create the file
+                fs.open(TIMELOG, 'w+', function(err, fd) {
+                    if (err)
+                        throw err;
+                    var data = {};
+                    data.log = [];
+                    data.checkedin = false;
+                    fs.close(fd);
+                    console.log("created timelog.txt");
+                    updateLog(data);
+                });
+            }
+            rl.close();
         });
+    } else {
+        fs.readFile(TIMELOG, function(err, data) {
+            if (err) throw err;
+            var log = JSON.parse(data);
+            updateLog(log);
+        });
+        rl.close();
     }
 });
 
+function updateLog(log) {
+    log.checkedin = !log.checkedin
+    log.log.push(new Date());
+    console.log("updated log...");
+    fs.writeFile(TIMELOG, JSON.stringify(log));
+}
